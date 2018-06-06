66081b800977dfe3de0948ee71623d6ea658c0e5
Emily Jennings <emily.jennings@flatironschool.com>
2018-06-06 00:54:40 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -45,28 +45,27 @@ def full?(board)
 end
 
 def draw?(board)
-  # if board.each{|place| (place == "X" || place == "O") && !won?(board)}
-  #   return true
-  # elsif won?(board)
-  #   return false
-  # elsif board.detect{|place| place == " "}
-  #   return false
-  # end
   full?(board) && !won?(board)
 end
 
 def over?(board)
-  if draw?(board)
-    true
-  elsif won?(board) && full?(board)
-    true
-  elsif won? && !full?
-    true
-  elsif !won?(board) && !full?(board)
-    false
-  end
+  draw?(board) || won?(board)
 end
 
 def winner(board)
+  WIN_COMBINATIONS.detect do |win_combination| #goes through each individual arrays in the nested array
+    win_index_1 = win_combination[0] # gives backthe numbers in the arrays
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if ("#{position_1}" == "X" && "#{position_2}" == "X" && "#{position_3}" == "X")
+      return "X"
+    elsif ("#{position_1}" == "O" && "#{position_2}" == "O" && "#{position_3}" == "O")
+      return "O"
+    end
+  end
 end
