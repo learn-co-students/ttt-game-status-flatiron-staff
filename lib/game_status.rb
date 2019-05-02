9723317408ae9c717cc7350d4c6d3fada561ec3f
Eli Kim <eli.kim@flatironschool.com>
2019-05-02 19:17:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,40 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+# WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left column
+  [1,4,7], # middle column
+  [2,5,8], # right column
+  [0,4,8], # diagonal
+  [2,4,6]  # diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win_comb|
+    board[win_comb[0]] == board[win_comb[1]] && board[win_comb[0]] == board[win_comb[2]] && position_taken?(board, win_comb[0])
+  end
+end
+
+def full?(board)
+  board.all? do |spot|
+    spot == "X" || spot == "O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board).first]
+  end
+end
